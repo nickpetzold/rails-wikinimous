ddf8ab7ee6c3a93be817ac69eaf0637832cb551b
Nick Petzold <nickpetzold@gmail.com>
2018-11-27 19:47:54 +0000

    Finish wikimonious

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+# Articles controller definition
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to :root
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
